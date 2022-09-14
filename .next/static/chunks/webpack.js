dff74325ce4159e0cebfc86da6279790184a0658
Ramazan E <ramazan@Ramazan-MacBook-Air.local>
2022-09-14 12:35:18 +0300

    read me eklendi

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59e6294b388e7e36"; }
+/******/ 		__webpack_require__.h = function() { return "ee67c11f122a85e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,18 +10,18 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="tr">
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Vie onepage themeforest"
+            content="Web Uygulamalamaları Geliştirme, Web Tasarım, Sosyal Medya, SEO"
           />
           <meta
             name="description"
-            content="RAsoft Yazılım"
+            content="RAsoft Teknoloji, Yazılım ve Danışmanlık"
           />
-          <meta name="author" content="" />
+          <meta name="author" content="RAsoft Teknoloji, Yazılım ve Danışmanlık" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
