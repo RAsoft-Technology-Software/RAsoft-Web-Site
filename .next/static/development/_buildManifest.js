6cc5ed8af13d7abf4c344e5898ce356e71523b64
necip <43618473+ncpy@users.noreply.github.com>
2022-09-15 17:16:16 +0300

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/404":["static\u002Fchunks\u002Fpages\u002F404.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/works2-dark":["static\u002Fchunks\u002Fpages\u002Fworks2-dark.js"],sortedPages:["\u002F404","\u002F_app","\u002F_error","\u002Fworks2-dark"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/web-uygulamalari-gelistirme":["static\u002Fchunks\u002Fpages\u002Fweb-uygulamalari-gelistirme.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fweb-uygulamalari-gelistirme"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> ef0e83a9ce771f229e144590a25edc9ea7963ccf
->>>>>>> 52f622b32680c33be0c1fde2914cf0eb2c1bf2af
