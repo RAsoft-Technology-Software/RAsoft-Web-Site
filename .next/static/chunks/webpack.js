79f74f55206bf9b29779772e75201afc7e24db42
= <ramazan.ertnc@gmail.com>
2022-09-15 12:20:32 +0300

    logo büyütüldü.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3332cba6f885f8a7"; }
+/******/ 		__webpack_require__.h = function() { return "ccc08c23407deaf5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
